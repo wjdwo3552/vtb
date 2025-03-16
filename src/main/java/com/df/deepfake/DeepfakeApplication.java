d330a443a480248869d67c700c693340c41d5af2
wjdwo <wjdwo3552@gmail.com>
2025-03-16 20:11:44 +0900

    초기 설정

---
+++ src/main/java/com/df/deepfake/DeepfakeApplication.java
@@ -0,0 +1,13 @@
+package com.df.deepfake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepfakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepfakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/df/deepfake/DeepfakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.df.deepfake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepfakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
