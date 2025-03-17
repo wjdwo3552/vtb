8be827f310bb4617b79ad64e74fbac785d535dcb
sngmin <lsgmin@naver.com>
2025-03-17 14:20:09 +0900

    init

---
+++ src/main/java/com/deepfake/df/DfApplication.java
@@ -0,0 +1,13 @@
+package com.deepfake.df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepfake/df/DfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepfake.df;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
