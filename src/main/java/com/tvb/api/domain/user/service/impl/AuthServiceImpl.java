92f516fcb0c2833026d27bea62cb173695beb00c
sngmin <lsgmin@naver.com>
2025-03-23 13:33:35 +0900

    아이디 또는 비밀번호 불일치 예외처리

--- src/main/java/com/tvb/api/domain/user/service/impl/AuthServiceImpl.java
+++ src/main/java/com/tvb/api/domain/user/service/impl/AuthServiceImpl.java
@@ -1,19 +1,15 @@
 package com.tvb.api.domain.user.service.impl;
 
 import com.tvb.api.domain.user.dto.LoginRequest;
-import com.tvb.api.domain.user.dto.RegisterRequest;
 import com.tvb.api.domain.user.dto.TokenResponse;
 import com.tvb.api.domain.user.entity.User;
+import com.tvb.api.domain.user.exception.InvalidCredentialsException;
 import com.tvb.api.domain.user.repository.UserRepository;
 import com.tvb.api.domain.user.service.AuthService;
 import com.tvb.api.jwt.security.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 
@@ -27,12 +23,13 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public TokenResponse makeTokenAndLogin(LoginRequest loginRequest) {
         Optional<User> user = userRepository.findByEmail(loginRequest.getEmail());
+
         if (user.isPresent() && passwordEncoder.matches(loginRequest.getPassword(), user.get().getPassword())) {
             Map<String, String> dataMap = loginRequest.getDataMap();
             String accessToken = jwtUtil.createToken(dataMap, 10);
             String refreshToken = jwtUtil.createToken(dataMap, 10);
             return new TokenResponse(accessToken,refreshToken,dataMap.get("email") );
         }
-        return null;
+        throw new InvalidCredentialsException();
     }
 }
