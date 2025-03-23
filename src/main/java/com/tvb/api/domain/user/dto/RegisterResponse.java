477f8767546905db8da344938161631e97f6d368
sngmin <lsgmin@naver.com>
2025-03-23 12:27:28 +0900

    회원가입 후 반환 응답 DTO

---
+++ src/main/java/com/tvb/api/domain/user/dto/RegisterResponse.java
@@ -0,0 +1,10 @@
+package com.tvb.api.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RegisterResponse {
+    private String uid;
+}
