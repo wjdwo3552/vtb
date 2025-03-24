7c5a2d0730969a94c694d76b6e12a9b99caf5a27
sngmin <lsgmin@naver.com>
2025-03-24 09:17:40 +0900

    로그인 타입을 수평적으로 확장하기 위한 변환 작업

---
+++ src/main/java/com/tvb/api/domain/user/entity/member/logintype/LoginTypeConvertor.java
@@ -0,0 +1,23 @@
+package com.tvb.api.domain.user.entity.member.logintype;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class LoginTypeConvertor implements AttributeConverter<LoginType, Integer> {
+    @Override
+    public Integer convertToDatabaseColumn(LoginType loginType) {
+        if (loginType == null) {
+            return null;
+        }
+        return loginType.getValue();
+    }
+
+    @Override
+    public LoginType convertToEntityAttribute(Integer dbData) {
+        if (dbData == null) {
+            return null;
+        }
+        return LoginType.fromValue(dbData);
+    }
+}
