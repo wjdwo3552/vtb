0341e3427fedbd975311d58b3a7067fbdf7a7400
sngmin <lsgmin@naver.com>
2025-03-26 22:20:03 +0900

    fix: 쿠키 secure 설정 3
    close:#12

--- src/main/java/com/tvb/api/jwt/config/SecurityConfig.java
+++ src/main/java/com/tvb/api/jwt/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.setAllowedOriginPatterns(List.of("tvb-front.vercel.app"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         corsConfiguration.setAllowCredentials(true);
